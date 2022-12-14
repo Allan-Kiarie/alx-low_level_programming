cc7ac03c3fff370a4ab192f0db8ac1204ee04267
C. Allan <allan.kiarie91@gmail.com>
2022-12-14 06:24:59 -0800

    Check for alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a letter is an alphabet
+ *
+ * @c: A character in the ASCII code
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
