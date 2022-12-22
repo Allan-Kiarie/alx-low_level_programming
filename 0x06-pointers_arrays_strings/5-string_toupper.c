57bbd133be493271fc53de832a1396b910a74277
C. Allan <allan.kiarie91@gmail.com>
2022-12-22 07:00:16 -0800

    Change any lowercase characters to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,12 +14,12 @@ char *string_toupper(char *)
 
 	while (s[i] != 0)
 	{
-		if (s[i] >= 97 && s[i] <= 122)
+		if ((s[i] >= 97) && (s[i] <= 122))
 		{
 			s[i] = s[i] - 32;
 		}
 		i++
 	}
 
-	return ();
+	return (s);
 }
