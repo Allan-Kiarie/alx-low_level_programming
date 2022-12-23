4b7949a1307af1bef5f80406d7addf2cf728ac3a
C. Allan <allan.kiarie91@gmail.com>
2022-12-23 02:36:32 -0800

    hot fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,29 @@
 
 char *cap_string(char *s)
 {
-	int i, j;
+	int i = 0, j;
 	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (i = 0; s[i] != '\0'; i++)
+	if ((s[i] >= 97) && (s[i] <= 122))
 	{
-		if ((s[i] >= 92) && (s[i] <= 122))
+		s[i] = s[i] - 32;
+	}
+	i++;
+
+	while (s[i] != '\0')
+	{
+		for (i = 0; i < 13; i++)
 		{
-			s[i] = s[i] - 32;
-		}
-		for (j = 0; j < 13; j++)
-		{
-			if (s[i] == separators[j])
+			if (s[j] == separators[i])
 			{
-				continue;
+				if ((s[j + 1] >= 97) && (s[j + 1] <= 122))
+				{
+					s[j + 1] = s[j + 1] - 32;
+				}
+				break;
 			}
 		}
+		j++;
 	}
-
 	return (s);
 }
