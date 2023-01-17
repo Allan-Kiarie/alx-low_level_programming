9d3c5e84e78249b1cea7c82939c7a03352b32c43
C. Allan <allan.kiarie91@gmail.com>
2023-01-17 12:08:02 -0800

    Create dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - a structure that stores some info about a dog
+ * @name: name of the dog
+ * @age: its age
+ * @owner: name of its owner
+ * Description: struct called "dog" that stores its name, age
+ * and name of its owner
+ */
 struct dog
 {
 	char *name;
