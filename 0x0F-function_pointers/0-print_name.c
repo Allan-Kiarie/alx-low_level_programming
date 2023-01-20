f45fee7d40b1223a5b0e0a1d747b57a03e85e5ca
C. Allan <allan.kiarie91@gmail.com>
2023-01-20 02:14:23 -0800

    Write a program that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,12 @@
 #include "function_pointers.h"
-
 /**
  * print_name - prints a name
- *
- * @name: - person's name
- *
+ * @name: person's name
  * @f: function pointer
- *
- * Return: nothing
+ * Return: no return
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-	{
 		f(name);
-	}
 }
