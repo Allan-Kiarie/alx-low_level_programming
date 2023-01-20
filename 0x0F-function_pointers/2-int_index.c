f4efffd9e16d80bb7b35d7d4eec24a95d4be6077
C. Allan <allan.kiarie91@gmail.com>
2023-01-20 02:53:55 -0800

    Add solution for 0x0F - 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers"
+/**
+ * int_index - searches for an integer
+ * @array: input array
+ * @size: size of array
+ * @cmp: function pointer
+ * Return: index of integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		if ( size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			if (c(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
