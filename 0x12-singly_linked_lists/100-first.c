950205630dca3aa2b680209f0dcf2abb6043ef87
C. Allan <allan.kiarie91@gmail.com>
2023-01-31 08:48:56 -0800

    update 0x12 - task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * before_main - prints a message before main fuction is executed
  * Return: no return
  */
-void before_main() __attribute__ ((constructor))
+void __attribute__ ((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
