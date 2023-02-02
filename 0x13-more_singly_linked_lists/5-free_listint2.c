462f54306ab3ad53d7d7199780d5ac4acc25e2e1
C. Allan <allan.kiarie91@gmail.com>
2023-02-02 05:23:54 -0800

    update solution for 0x13 - task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,14 +13,12 @@ void free_listint2(listint_t **head)
 	if (head != NULL)
 	{
 		current = *head;
-
 		while (current != NULL)
 		{
 			temp = current;
 			current = current->next;
 			free(temp);
 		}
-
 		*head = NULL;
 	}
-}		
+}
