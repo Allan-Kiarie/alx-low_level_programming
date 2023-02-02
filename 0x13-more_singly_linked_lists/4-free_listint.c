b2e9f40c776b80d933b94e2f70a1ed1bf015c20e
C. Allan <allan.kiarie91@gmail.com>
2023-02-02 04:42:29 -0800

    Add solution to 0x13 - task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: start of listint_t
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->n);
+		free(current);
+	}
+}
