18c2ee8f4562ee9702d3b57c5b8cf82f30d59d15
C. Allan <allan.kiarie91@gmail.com>
2023-02-03 00:55:11 -0800

    Update solution to 0x13 - task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,9 +13,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newNode, *current;
 
 	current = *head;
-	if (idx != 0 && current != NULL)
+	if (idx != 0)
 	{
-		for (i = 0; i < idx - 1; i++)
+		for (i = 0; i < idx - 1 && current != NULL; i++)
 			current = current->next;
 	}
 
