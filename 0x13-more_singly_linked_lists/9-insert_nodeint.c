2bc0f5382717feb8ade7ac9697c00d9935ed45cd
C. Allan <allan.kiarie91@gmail.com>
2023-02-03 00:38:26 -0800

    Add solution to 0x13 - task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: start of the linked list
+ * @idx: index of the list where the new node should be added
+ * @n: data of the new node
+ * Return: the new node, or NULL if failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *newNode, *current;
+
+	current = *head;
+	if (idx != 0 && current != NULL)
+	{
+		for (i = 0; i < idx - 1; i++)
+			current = current->next;
+	}
+
+	if (idex != 0 && current == NULL)
+		return (NULL);
+	
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+	
+	newNode->n = n;
+
+	if (idex == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+	}
+	esle
+	{
+		newNode->next = current->next;
+		current->next = newNode;
+	}
+
+	return (newNode);
+}
