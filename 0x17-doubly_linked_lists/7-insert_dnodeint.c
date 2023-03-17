015802619c416846c7cbd80c14f817e018e5488b
Allan Kiarie <allan.kiarie91@gmail.com>
2023-03-17 07:52:58 -0700

    Add solution to task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -48,11 +48,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				temp->next->prev = newNode;
 			newNode->prev = temp;
 			temp->next = newNode;
-			
+
 			return (newNode);
 		}
 	}
-	
+
 	free(newNode);
 	return (NULL);
-}	
+}
