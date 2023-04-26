a09923cc75be13ed1d5cc88e67995b19851226d0
Allan Kiarie <allan.kiarie91@gmail.com>
2023-04-26 11:35:55 -0700

    Sum of even-value fibonacci numbers < 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of even-valued fibonacci numbers
+ * below 4000000
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int num1, num2, fib, sum;
+	num1 = 1;
+	num2 = 2;
+	fib = 0;
+	sum = num2;
+
+	while (fib < 4000000)
+	{
+		fib = num1 + num2;
+		num1 = num2;
+		num2 = fib;
+
+		if (fib % 2 == 0)
+		{
+			sum += fib;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
