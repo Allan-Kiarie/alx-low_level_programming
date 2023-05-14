0b6df5db706ceccb83f5c0174bfe9cb891a3662e
Allan Kiarie <allan.kiarie91@gmail.com>
2023-05-14 09:53:02 -0700

    add code that checks for overflow

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdbool.h>
+#include <limits.h>
+
 /**
  * _atoi - converts a string to an integer
  * @s: input string
@@ -6,16 +9,19 @@
  */
 int _atoi(char *s)
 {
-	int result, sign, i;
+	int result, sign, i, digit;
+	bool isNegative;
 
 	result = 0;
 	sign = 1;
 	i = 0;
+	isNegative = false;
 
 	while (s[i] != '\0' && (s[i] < 48 || s[i] > 57))
 	{
 		if (s[i] == '-')
 		{
+			isNegative = true;
 			sign *= -1;
 		}
 		i++;
@@ -23,7 +29,17 @@ int _atoi(char *s)
 
 	while (s[i] >= 48 && s[i] <= 57)
 	{
-		result = result * 10 + (s[i] - 48);
+		digit = s[i] - 48;
+
+		if (result > (INT_MAX - digit) / 10)
+		{
+			if (isNegative)
+				return (INT_MIN);
+			else
+				return (INT_MAX);
+		}
+
+		result = result * 10 + digit;
 		i++;
 	}
 
