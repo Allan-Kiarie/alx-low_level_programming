17d71dc5914f8740891c29192606fbb7c5e4f07e
Allan Kiarie <allan.kiarie91@gmail.com>
2023-05-14 23:11:00 -0700

    A program that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @src: input string
+ * Return: pointer to the encoded string
+ */
+
+char *rot13(char * src)
+{
+	int i, j;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (src[i] == alpha[j])
+			{
+				src[i] = rot[j];
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (src);
+}
