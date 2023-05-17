b4ccaeba27b44cabfdd7d62f229d8890e46abfa2
Allan Kiarie <allan.kiarie91@gmail.com>
2023-05-16 23:40:07 -0700

    print a buffer

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -16,37 +16,30 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	len2 = 0;
 	carry = 0;
 
-	/*Calculate the lengths of the input strings n1 and n2*/
 	while (*(n1 + len1) != '\0')
 		len1++;
 	while (*(n2 + len2) != '\0')
 		len2++;
 
-	/*Determine the larger length between n1 and n2*/
 	if (len1 >= len2)
 		maxlen = len1;
 	else
 		maxlen = len2;
 
-	/*check if the result string r has enough space to store the result*/
 	if (size_r <= maxlen + 1)
 		return (0);
 
-	/*Null-terminate the result string*/
 	r[maxlen + 1] = '\0';
 
-	/*Decrement the counters and size*/
 	len1--;
 	len2--;
 	size_r--;
 
-	/*Convert the last characters of n1 and n2 to integers*/
 	num1 = *(n1 + len1) - 48;
 	num2 = *(n2 + len2) - 48;
 
 	while (maxlen >= 0)
 	{
-		/*perform additioin of digits and add carry if necessary*/
 		add = num1 + num2 + carry;
 
 		if (add >= 10)
@@ -54,13 +47,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			carry = 0;
 
-		/*store the digit in the result string*/
 		if (add > 0)
 			*(r + maxlen) = (add % 10) + 48;
 		else
 			*(r + maxlen) = '0';
 
-		/*Update the digits and counters for n1 and n2*/
 		if (len1 > 0)
 		{
 			len1--;
@@ -75,12 +66,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		} else
 			num2 = 0;
 
-		/*Decrement counters and size_r*/
 		maxlen--;
 		size_r--;
 	}
 
-	/*check if the result has leading zeros and adjust accordingly*/
 	if (*(r) == '0')
 		return (r + 1);
 	else
