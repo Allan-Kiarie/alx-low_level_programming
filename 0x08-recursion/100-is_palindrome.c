19825fcad7d063bff96b5c8d21ba99fd7c1f3ae9
Allan Kiarie <allan.kiarie91@gmail.com>
2023-05-18 23:19:07 -0700

    Remove loops in task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,25 +1,44 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion - determines length of a string
+ * @s: input string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * is_palindrome_helper - compares characters at both ends of string
+ * @s: input string
+ * @start: lowest count
+ * @end: highest count
+ * Return: 1 if characters are same, 0 if not
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+
+	if (*(s + start) != *(s + end))
+		return (0);
+
+	return (0 + is_palindrome_helper(s, start + 1, end - 1));
+}
+
+/**
  * is_palindrome - checks if a string is a palindrome
  * @s: input string
  * Return: 1 if a string is a palindrome and 0 if not
  */
 int is_palindrome(char *s)
 {
-	int len, i, j;
-
-	len = 0;
-	while (*(s + len) != '\0')
-		len++;
-
-	if (len == 0)
+	if (*s == '\0')
 		return (1);
-
-	for (i = 0, j = len - 1; i < j; i++, j--)
-	{
-		if (*(s + i) != *(s + j))
-			return (0);
-	}
-	return (1);
+	return (is_palindrome_helper(s, 0, _strlen_recursion(s) - 1));
 }
